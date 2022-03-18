69a8c894ff43097eea6d1f740fd1762617076c10
Maxone52 <Maxwell-52@mail.ru>
2022-03-18 08:02:47 +0500

    end

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -33,16 +33,17 @@ public class CategoryController : Controller
             ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
         }
 
-        if(ModelState.IsValid)
+        if (ModelState.IsValid)
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
         }
         return View(obj);
     }
 
-    [HttpGet]
+
     public IActionResult Edit(int? id)
     {
         if (id == null || id == 0)
@@ -69,8 +70,38 @@ public class CategoryController : Controller
         {
             _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
             return RedirectToAction("Index");
         }
         return View(obj);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+            return NotFound();
+
+        var categoryFromDb = _db.Categories.Find(id);
+
+        if (categoryFromDb == null)
+            return NotFound();
+
+        return View(categoryFromDb);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+        if (obj == null)
+            return NotFound();
+
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
+
     }
 }
